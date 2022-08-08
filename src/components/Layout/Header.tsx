2f1861907c806e5df66ec2f4abb230373270d45c
Jefferson Dias <jefer.ld@hotmail.com>
2022-08-07 22:24:49 -0300

    Header component adjustment

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -13,14 +13,16 @@ const Header = () => {
     return(
       <>
       <div className="container-fluid bg-light header">
-        <h5 className="title">Dog Breed</h5>
-        <div>
-          <button onClick={logoff} className="btn btn-sm btn-primary">Logout</button>
+        <div className="header-content container mx-auto">
+          <h5 className="title">Dog Breed</h5>
+          <div>
+            <button onClick={logoff} className="btn btn-sm btn-primary">Logout</button>
+          </div>
         </div>
-        
+
       </div>
-      <div className="menu mx-auto">
-          <ul>
+      <div className="menu mx-auto container">
+          <ul className="">
             <li><Link className={`${disabledLink("chihuahua") && "disabled-link"}`} to={`/breeds/chihuahua`}>Chihuahua</Link></li>
             <li><Link className={`${disabledLink("husky") && "disabled-link"}`} to={`/breeds/husky`}>Husky</Link></li>
             <li><Link className={`${disabledLink("labrador") && "disabled-link"}`} to={`/breeds/labrador`}>Labrador</Link></li>
